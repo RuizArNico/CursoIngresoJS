206e863b3bfad2c502b6dc1d9d67906acd625957
Nicolas Ruiz <nruiz@Nicolass-MacBook-Pro.local>
2021-07-03 17:06:44 -0300

    Ejercicios E/S 1,2,3.

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,17 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	// var nombreIngresado;
+	// nombreIngresado=txtIdNombre.value;
+	// alert(nombreIngresado);
 
+	// Alternativa del codigo, utilizamos document y getElementById para traer el value.
+	
+	let nombreIngresado;
+
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	alert(nombreIngresado);
 }
 
 
