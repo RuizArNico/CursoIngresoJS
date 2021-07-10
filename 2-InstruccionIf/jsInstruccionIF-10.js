7b575b5a18ad0a738d4847db516f228533322468
Nicolas Ruiz <nicolas.ruiz.1992@gmail.com>
2021-07-10 20:58:05 -0300

    Agrego comentarios sobre Math.floor()  y Math.random()

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -3,7 +3,12 @@ function mostrar()
 	let numRandom;
 
 	//Genero el número RANDOM entre 1 y 10 
-	numRandom = Math.floor(Math.random()* 10) + 1 ;
+	// Math.random() * (maximo - minimo) - minimo;
+	// Ejemplo --------(  10   -   1   ) - 1     ;
+	// Ejemplo2 Math.random() * 10 - 1; Al poner el numero 10, toma los valores de: (0 a 9) + 1;
+	// Math.floor lleva el numero entero anterior. Ej: 6.234 o 6.987 es igual a 6.
+
+	numRandom = Math.floor(Math.random() * 10) + 1 ;
 
 	if( numRandom == 9 || numRandom == 10 ){
 
