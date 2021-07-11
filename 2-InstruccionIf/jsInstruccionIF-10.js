9c92c4cacbccb4919a4c6911e4f9dbd7ebb52b67
Nicolas Ruiz <nicolas.ruiz.1992@gmail.com>
2021-07-11 20:24:11 -0300

    modificacion en el ejercicio 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -4,21 +4,21 @@ function mostrar()
 
 	//Genero el número RANDOM entre 1 y 10 
 	// Math.random() * (maximo - minimo) - minimo;
-	// Ejemplo --------(  10   -   1   ) - 1     ;
-	// Ejemplo2 Math.random() * 10 - 1; Al poner el numero 10, toma los valores de: (0 a 9) + 1;
+	// Ejemplo --------(  10   -   1   ) -   1   ;
+	
 	// Math.floor lleva el numero entero anterior. Ej: 6.234 o 6.987 es igual a 6.
 
-	numRandom = Math.round(Math.random() * 10+1) ;
+	numRandom = Math.round(Math.random() * 10 + 1) ;
 
 	if( numRandom == 9 || numRandom == 10 ){
 
 		alert("EXCELENTE. Nota: " + numRandom);
 
-	}else if( numRandom > 4 && numRandom <= 8 ){
+	}else if( numRandom >= 4 && numRandom <= 8 ){
 
 		alert("APROBÓ. Nota: " + numRandom);
 
-	}else if( numRandom <= 4){
+	}else if( numRandom < 4){
 
 		alert("Vamos, la proxima se puede. Nota: " + numRandom);
 
