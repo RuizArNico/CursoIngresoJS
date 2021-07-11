09de78034f3ef4a14c25c504fa4ddbce5e739f32
Nicolas Ruiz <nicolas.ruiz.1992@gmail.com>
2021-07-10 21:57:55 -0300

    Modificacion Math.floor a Math.round

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -8,7 +8,7 @@ function mostrar()
 	// Ejemplo2 Math.random() * 10 - 1; Al poner el numero 10, toma los valores de: (0 a 9) + 1;
 	// Math.floor lleva el numero entero anterior. Ej: 6.234 o 6.987 es igual a 6.
 
-	numRandom = Math.floor(Math.random() * 10) + 1 ;
+	numRandom = Math.round(Math.random() * 10+1) ;
 
 	if( numRandom == 9 || numRandom == 10 ){
 
