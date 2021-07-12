e2a91d7db7f01aa9e10b19804ec0d11da3874d8b
Nicolas Ruiz <nicolas.ruiz.1992@gmail.com>
2021-07-11 22:18:01 -0300

    Ejercicios 1 a 5 de Switch realizados.

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,23 @@
 function mostrar()
 {
+	let mes;
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	mes = document.getElementById("txtIdMes").value;
 
+	switch(mes){
 
+		case "Enero":
+			alert("Que comiences bien el año!!!.");
+			break;
+		case "Marzo":
+			alert("A clases!!!.");
+			break;
+		case "Julio":
+			alert("Se vienen las vacaciones!!!.");
+			break;
+		case "Diciembre":
+			alert("Felices fiesta!!!.");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,9 +1,37 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
 	
+	//tomo el mes
+	mes = document.getElementById("txtIdMes").value;
+	
+	switch(mes){
+
+		case "Febrero":
+
+			alert("Tiene 28 dias.");
+			break;
+
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+
+			alert("Tiene 30 dias.");
+			break;
+			
+		// case "Enero":
+		// case "Marzo":
+		// case "Mayo":
+		// case "Julio":
+		// case "Agosto":
+		// case "Octubre":
+		// case "Diciembre":
+		default:
+			
+			alert("Tiene 31 dias.");
+			break;
+	}
 	
 
 
