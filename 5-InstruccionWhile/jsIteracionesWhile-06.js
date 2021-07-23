a290c6e1f760162007a3f4ae3eeee24d7c561377
Nicolas Ruiz <nicolas.ruiz.1992@gmail.com>
2021-07-23 18:19:51 -0300

    Ejercicios 06 y 07 de While realizados

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,3 +1,10 @@
+/*
+Autor: Ruiz Armella Juan Nicolas
+Ejercicio 06 While
+
+Al presionar el botón pedir 5 números e informar
+la suma acumulada y el promedio.
+*/
 function mostrar()
 {
 	var contador;
@@ -6,8 +13,15 @@ function mostrar()
 
 	contador=0;
 	acumulador=0;
-	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+
+	while(contador != 5)
+	{
+		contador= contador + 1;
+		numeroIngresado=prompt('Ingrese un numero: ' + '(Posicion: ' +contador+')');
+		numeroIngresado=parseInt(numeroIngresado);
+		acumulador= acumulador + numeroIngresado;
+	}
+
+	document.getElementById('txtIdSuma').value=acumulador;
+	document.getElementById('txtIdPromedio').value=acumulador/5;
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,4 +1,7 @@
 /*
+Autor: Ruiz Armella Juan Nicolas
+Ejercicio 07 While
+
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
@@ -7,12 +10,21 @@ function mostrar()
 	var contador;
 	var acumulador;
 	var respuesta;
+	var numeroIngresado;
 	contador=0;
 	acumulador=0;
 	respuesta='si';
 
+	while(respuesta == 'si')
+	{
+		contador= contador + 1;
+		numeroIngresado=prompt('Ingrese un numero: ' + '(Posicion: ' +contador+')');
+		numeroIngresado=parseInt(numeroIngresado);
+		acumulador= acumulador + numeroIngresado;
+		respuesta=prompt('Desea continuar? ingrese "si" o "no"');
+	}
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+	document.getElementById('txtIdSuma').value=acumulador;
+	document.getElementById('txtIdPromedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
