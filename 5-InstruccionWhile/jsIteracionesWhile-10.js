8eddc253023e9ccb3171eac1fe1ec2d048a2e0a5
Nicolas Ruiz <nicolas.ruiz.1992@gmail.com>
2021-07-23 02:24:01 -0300

    Ejercicio While 10 realizado

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -1,4 +1,7 @@
 /*
+Autor: Ruiz Armella Juan Nicolas
+Ejercicio 10 While
+
 Al presionar el botón pedir  números  
 hasta que el usuario quiera, mostrar:
 1-Suma de los negativos.
@@ -13,17 +16,72 @@ hasta que el usuario quiera, mostrar:
 function mostrar()
 {
 	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
-
-	respuesta="si";
+	var respuesta; var numeroIngresado; var sumaNegativos; var sumaPositivos; var contadorPositivo; var promedioPositivo;
+	var promedioNegativo; var pares; var diferenciaPositivoNegativo; var contadorNegativo; var contadorCero;
+	respuesta="si"; 
+	sumaPositivos=0;
+	sumaNegativos=0;
+	contadorPositivo=0;
+	contadorNegativo=0;
+	contadorCero=0;
 
 	while(respuesta=="si")
 	{
-		
+		numeroIngresado=prompt('Ingrese un numero: ');
+		numeroIngresado=parseFloat(numeroIngresado);
+		if(numeroIngresado>0)
+		{
+			//2-Suma de los positivos.
+			sumaPositivos=sumaPositivos+numeroIngresado;
+			//3-Cantidad de positivos.
+			contadorPositivo=contadorPositivo+1;
+		}
+		else
+		{
+			if(numeroIngresado<0)
+			{
+				//1-Suma de los negativos.
+				sumaNegativos=sumaNegativos+numeroIngresado;
+				//4-Cantidad de negativos.
+				contadorNegativo=contadorNegativo+1;
+			}
+			else
+			{
+				//5-Cantidad de ceros.
+				contadorCero=contadorCero+1;
+			}
+		}
 		respuesta=prompt("desea continuar?");
 	}//fin del while
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+	//7-Promedio de positivos.
+	promedioPositivo=sumaPositivos/contadorPositivo;
+	//8-Promedios de negativos.
+	promedioNegativo=sumaNegativos/contadorNegativo;
+	//9-Diferencia entre positivos y negativos, (positvos-negativos).
+	diferenciaPositivoNegativo=sumaPositivos-sumaNegativos;
+	//6-Cantidad de números pares.
+	pares=diferenciaPositivoNegativo%2;
+	if(pares==0)
+	{
+		pares=diferenciaPositivoNegativo/2;
+	}
+	else
+	{
+		pares=diferenciaPositivoNegativo-1;
+		pares=pares/2;
+	}
+
+	document.write("la suma de negativos es : "+sumaNegativos);// 1-Suma de los negativos.
+	document.write("<br>la suma de positivos es : "+sumaPositivos);// 2-Suma de los positivos.
+	document.write("<br>la cantidad de positivos es : "+contadorPositivo);// 3-Cantidad de positivos.
+	document.write("<br>la cantidad de negativos es : "+contadorNegativo);// 4-Cantidad de negativos.
+	document.write("<br>la cantidad de ceros es : "+contadorCero);// 5-Cantidad de ceros.
+	document.write("<br>la cantidad de numeros pares es : "+pares);// 6-Cantidad de números pares.
+	document.write("<br>el promedio positivo es : "+promedioPositivo);// 7-Promedio de positivos.
+	document.write("<br>el promedio negativos es : "+promedioNegativo);// 8-Promedios de negativos.
+	document.write("<br>la diferencia entre positivos y negativos es : "+diferenciaPositivoNegativo);// 9-Diferencia entre positivos y negativos, (positvos-negativos).
 }//FIN DE LA FUNCIÓN
+// console.log(-324%2);
+// console.log(-324/2);
+// console.log(387-(-324));
